e5d93ab000d6ae90129ceef77fd681bd8e578620
JoaoDionisio <jd1583@hotmail.com>
2021-10-09 10:25:30 -0300

    Comentando funcoesRetorno.py

--- funcoesRetorno.py
+++ funcoesRetorno.py
@@ -5,7 +5,7 @@ def par_impar():
     numero = 5000
     if (numero % 2) == 0:
         return "O número é par."
-    return "O número é ímpar."
+    return "O número é ímpar."  # Do you see? I don't need one else here. 
 
 
 print(par_impar())
