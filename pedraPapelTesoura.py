effe427c7398d2eb76c2fb345351a383be6298e3
JoaoDionisio <jd1583@hotmail.com>
2021-10-13 17:13:19 -0300

    Construindo o jogo...PedraPapelTesoura

--- pedraPapelTesoura.py
+++ pedraPapelTesoura.py
@@ -1,15 +1,34 @@
 from rich import print
 
-#  Pedra Papel e Tesoura
+#  Pedra Papel e Tesoura - Game DANKI CODE
 ordem = True
 
-while ordem:
-    esc_jogador = input("Você quer jogar novamente?")
-    esc_jogador = esc_jogador.lower()
-    print(esc_jogador)
-    if esc_jogador in "sim":
-        print("Very well. Let's go start.")
 
-    if esc_jogador in "nao":
+def opcao_jogador():
+    opcao = input("Escolha Pedra, Papel ou Tesoura: ")
+    opcao.lower()
+    if opcao == "pedra":
+        return opcao
+    elif opcao == "papel":
+        return opcao
+    elif opcao == "tesoura":
+        return opcao
+    else:
+        print("Você nao digitou umas das opções válidas.")
+        opcao_jogador()
+
+
+while ordem:
+
+    opcao_jogador()
+
+    esc_jogador = input("Você quer jogar novamente?")
+    esc_jogador.lower()
+    print(esc_jogador)
+
+    if esc_jogador in "sim":
+        print("Very well. Let's start again.")
+
+    if esc_jogador in "naonão":
         ordem = False
         print(ordem)
