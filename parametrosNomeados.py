b11f03825e4fd7add71ff22a788c8f33837f88ac
JoaoDionisio <jd1583@hotmail.com>
2021-10-14 19:48:09 -0300

    Exercício parâmetros nomeados.

---
+++ parametrosNomeados.py
@@ -0,0 +1,10 @@
+from rich import print
+
+
+def imprimir_nome(nome, sobrenome, idade):
+    print(f"Nome: {nome}")
+    print(f"Sobrenome: {sobrenome}")
+    print(f"Idade: {idade}")
+
+
+imprimir_nome(sobrenome="Lucena", nome="Prince", idade=2)
