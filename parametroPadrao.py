139f7576768dffd12beb7c993e4dc00d35b8bb11
JoaoDionisio <jd1583@hotmail.com>
2021-10-15 11:40:08 -0300

    Exercicio parametroPadrao

--- parametroPadrao.py
+++ parametroPadrao.py
@@ -1,12 +1,17 @@
 from rich import print
 
 
-def imprimir_imovel(nomeImovel, n_quartos, vagasGaragem=None):
+def imprimir_imovel(nomeImovel="Apartamento - SP", n_quartos=3, vagasGaragem=None, *args):
     print(f"Título: {nomeImovel}")
     print(f"Número de quartos: {n_quartos}")
     if vagasGaragem is not None:
         print(f"Número de vagas: {vagasGaragem}")
 
 
+def imprimir_nomes(*nomes):
+    print(nomes)
+
+
 print("***"*30)
-imprimir_imovel("Casa para 3 pessoas - SP", 3, 2, 5)
+lista = ["Ana", "Beatriz", "Pedro"]
+imprimir_nomes(*lista)
