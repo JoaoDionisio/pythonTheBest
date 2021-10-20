38f35ad324c6aae079b30919863664df2d893d93
JoaoDionisio <jd1583@hotmail.com>
2021-10-20 16:18:51 -0300

    Modulo.py

---
+++ modulo.py
@@ -0,0 +1,17 @@
+#  import random Dessa forma você importa tudo o que tem dentro do módulo.
+#  Para ver o que tem dentro do módulo, segure ctrl + botão esquerdo do mouse.
+
+#  importa tudo do módulo.
+from random import randint
+from pacote.subPastaPacote import principalSubPasta as ps
+from pacote import (
+    principal as pr,
+    secundario as se,
+    outro as ot
+)
+
+print(randint(1, 5))
+print(pr.soma(5, 5))
+print(se.quadratica(5))
+print(ot.cubica(3))
+print(ps.multiplicacao(3, 5))
