0f5ccf9dbf8a9e1f72c31e3f68c34e1768a1b825
JoaoDionisio <jd1583@hotmail.com>
2021-10-24 15:28:32 -0300

    Modulo 001

--- modulo.py
+++ modulo.py
@@ -2,16 +2,18 @@
 #  Para ver o que tem dentro do módulo, segure ctrl + botão esquerdo do mouse.
 
 #  importa tudo do módulo.
+
+
 from random import randint
 from pacote.subPastaPacote import principalSubPasta as ps
 from pacote import (
-    principal as pr,
+    # principal as pr,
     secundario as se,
     outro as ot
 )
-
+from pacote.principal import soma as sm
 print(randint(1, 5))
-print(pr.soma(5, 5))
+print(sm(5, 5))
 print(se.quadratica(5))
 print(ot.cubica(3))
 print(ps.multiplicacao(3, 5))
